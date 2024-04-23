5642c0dbd57183fa6c08c0f00d68cf9d2a090d10
deepanshu-hh <deepanshu.a@hubhopper.com>
2024-04-23 13:52:02 +0530

    some coding practice

---
+++ InputOutput.kt
@@ -0,0 +1,24 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    var t = sc.nextInt()
+    while (t-- != 0) {
+        var n = sc.nextInt()
+        var s: String = sc.nextLine()
+        var list = s.toMutableList()
+        var i = 0
+        println(list)
+    }
+}
+
+
+// not able to solve questions
+// https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/ADJSUMPAR
+// https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/BREAKSTICK
+// https://www.codechef.com/practice/course/2-star-difficulty-problems/DIFF1500/problems/AVGPERM
+// https://www.codechef.com/practice/course/2-star-difficulty-problems/DIFF1500/problems/MTYFRI -> Counting swaping left/right
+// https://www.codechef.com/practice/course/2-star-difficulty-problems/DIFF1500/problems/CHFHEIST -> Arthmetic Progression
+// https://www.codechef.com/practice/course/2-star-difficulty-problems/DIFF1500/problems/COLOUR -> sorting required before secondary color check
+// https://www.codechef.com/practice/course/2-star-difficulty-problems/DIFF1500/problems/PLAYFIT -> just in confusion need practice
+
